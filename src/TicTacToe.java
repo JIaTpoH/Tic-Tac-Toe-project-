a56f91e4fc458a54186d0980bca5c850dc56678e
Dmitri Mandaji <badmopss@gmail.com>
2023-04-19 15:18:44 +0200

    feat: add checkForDraw fix: implements ActionListener

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class TicTacToe extends JFrame  {
+public class TicTacToe extends JFrame implements ActionListener {
   private final JButton[][] buttons;
   private char currentPlayer;
 
@@ -19,7 +19,7 @@ public class TicTacToe extends JFrame  {
       for (int j = 0; j < 3; j++) {
         buttons[i][j] = new JButton("-");
         buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 60));
-        buttons[i][j].addActionListener((ActionListener) this);
+        buttons[i][j].addActionListener(this);
         panel.add(buttons[i][j]);
       }
     }
@@ -40,9 +40,7 @@ public class TicTacToe extends JFrame  {
     }
   }
 
-  // Проверка на победу
   public boolean checkForWin() {
-    // Проверка строк
     for (int i = 0; i < 3; i++) {
       if (buttons[i][0].getText().equals(buttons[i][1].getText()) &&
           buttons[i][1].getText().equals(buttons[i][2].getText()) &&
@@ -51,7 +49,6 @@ public class TicTacToe extends JFrame  {
       }
     }
 
-    // Проверка столбцов
     for (int j = 0; j < 3; j++) {
       if (buttons[0][j].getText().equals(buttons[1][j].getText()) &&
           buttons[1][j].getText().equals(buttons[2][j].getText()) &&
@@ -60,7 +57,6 @@ public class TicTacToe extends JFrame  {
       }
     }
 
-    // Проверка диагоналей
     if (buttons[0][0].getText().equals(buttons[1][1].getText()) &&
         buttons[1][1].getText().equals(buttons[2][2].getText()) &&
         !buttons[0][0].getText().equals("-")) {
@@ -69,10 +65,27 @@ public class TicTacToe extends JFrame  {
     return buttons[0][2].getText().equals(buttons[1][1].getText()) &&
         buttons[1][1].getText().equals(buttons[2][0].getText()) &&
         !buttons[0][2].getText().equals("-");
-}
+  }
+
+
+  public boolean checkForDraw() {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (buttons[i][j].getText().equals("-")) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 
   public static void main(String[] args) {
     new TicTacToe();
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
   }
 }
 
